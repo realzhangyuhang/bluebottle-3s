485fbb71ca360fb2558c120bbafb7b210fcbfdbd
RaiNIng <zhngyuhng@gmail.com>
2020-01-01 17:37:40 -0600

    renamed s_beta in seeder.c

--- tools/src/seeder.c
+++ tools/src/seeder.c
@@ -233,7 +233,7 @@ void domain_read_input(void)
   fret = fscanf(infile, "nu %lf\n", &fbuf);
   fret = fscanf(infile, "s_D %lf\n", &fbuf);
   fret = fscanf(infile, "s_k %lf\n", &fbuf);
-  fret = fscanf(infile, "s_alpha %lf\n", &fbuf);
+  fret = fscanf(infile, "s_beta %lf\n", &fbuf);
   fret = fscanf(infile, "s_ref %lf\n", &fbuf);
   fret = fscanf(infile, "\n");
 
